3967ee3c42d96c2a029597b7a75c2d86b6270346
anhpt6852 <anh31081999>
2022-04-18 10:09:12 +0700

    init: register page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:ohio_templates/feature/home/presentation/home_page.dart';
 import 'package:ohio_templates/feature/login/presentation/login_page.dart';
+import 'package:ohio_templates/feature/register/presentation/register_page.dart';
 
 class AppRoutes {
   static const login = '/login-page';
+  static const register = '/register-page';
   static const home = '/home-page';
 }
 
 class AppRouter {
-  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+  static Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.login:
-        return MaterialPageRoute<dynamic>(
+        return MaterialPageRoute(
           builder: (_) => const LoginPage(),
           settings: settings,
         );
-        case AppRoutes.home:
-        return MaterialPageRoute<dynamic>(
+      case AppRoutes.register:
+        return MaterialPageRoute(
+          builder: (_) => const RegisterPage(),
+          settings: settings,
+        );
+      case AppRoutes.home:
+        return MaterialPageRoute(
           builder: (_) => const HomePage(),
           settings: settings,
         );
-      default:
-        return null;
     }
+    return null;
   }
 }
