e5f1c45eaf33a6e02cb70e170341cc5b43af932f
shunkou97 <shunkou97@gmail.com>
2022-04-19 15:12:35 +0700

    change UI

--- lib/feature/user_profile/presentation/user_profile_page.dart
+++ lib/feature/user_profile/presentation/user_profile_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ohio_templates/core/commons/presentation/common_button.dart';
 import 'package:ohio_templates/core/constant/colors.dart';
 import 'package:ohio_templates/feature/user_profile/presentation/widgets/profile_pic.dart';
-import 'package:ohio_templates/routes.dart';
+import 'package:ohio_templates/generated/locale_keys.g.dart';
 
 import 'widgets/user_profile_info.dart';
 
@@ -14,12 +13,13 @@ class UserProfilePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.ink[0],
       appBar: AppBar(
-        title: Text(),
+        title: const Text(LocaleKeys.profile_title),
       ),
       body: Column(
         children: const [
+          SizedBox(height: 16),
           ProfilePic(),
-          Divider(height: 25),
+          Divider(height: 24),
           UserProfileInfo(),
         ],
       ),
